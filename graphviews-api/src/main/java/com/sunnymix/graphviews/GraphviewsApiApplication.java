a93188035cea2379e812579823769a86d59c31fc
sunnymix <sunny@sunnymix.com>
2022-09-12 23:37:48 +0800

    添加api数据库配置和默认controller

--- graphviews-api/src/main/java/com/sunnymix/graphviews/GraphviewsApiApplication.java
+++ graphviews-api/src/main/java/com/sunnymix/graphviews/GraphviewsApiApplication.java
@@ -1,19 +1,31 @@
-package com.sunnymix.graphviews.api;
+package com.sunnymix.graphviews;
 
 import com.sunnymix.graphviews.common.Constant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author sunnymix
  */
 @SpringBootApplication(scanBasePackages = {Constant.BASE_PACKAGE})
 @ConfigurationPropertiesScan({Constant.BASE_PACKAGE})
-public class GraphviewsApiApp {
+public class GraphviewsApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GraphviewsApiApp.class, args);
+        SpringApplication.run(GraphviewsApiApplication.class, args);
+    }
+
+    @RestController
+    public static class DefaultController {
+
+        @GetMapping("/")
+        public String index() {
+            return Constant.APP_NAME;
+        }
+
     }
 
 }
