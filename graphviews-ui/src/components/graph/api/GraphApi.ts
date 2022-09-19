8622670fdff13403dda23d6f29d3dccb3a043d73
sunnymix <sunny@sunnymix.com>
2022-09-19 22:30:20 +0800

    查询和展示Graph详情

--- graphviews-ui/src/components/graph/api/GraphApi.ts
+++ graphviews-ui/src/components/graph/api/GraphApi.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import Constant from "@/components/common/Constant";
+import { GraphData } from "../graph/Graph";
+
+// --- query graph:
 
 const API_GRAPH_QUERY = Constant.API_HOST + "/graph/query";
 
-const queryGraph = (keyword: string, cb: Function) => {
+type QueryGraphCallback = (graph: GraphData[]) => void;
+
+const queryGraph = (keyword: string, cb: QueryGraphCallback) => {
   axios.get(`${API_GRAPH_QUERY}?keyword=${keyword}`)
     .then(res => {
       const graphs = res.data?.data || [];
@@ -11,6 +16,21 @@ const queryGraph = (keyword: string, cb: Function) => {
     });
 };
 
+// --- get graph:
+
+const API_GRAPH_GET = Constant.API_HOST + "/graph/get";
+
+type GetGraphCallback = (graph: GraphData|null) => void;
+
+const getGraph = (id: string, cb: GetGraphCallback) => {
+  axios.get(`${API_GRAPH_GET}?id=${id}`)
+    .then(res => {
+      const graph = res.data?.data || null;
+      cb(graph);
+    });
+};
+
 export default {
   queryGraph,
+  getGraph,
 };
--- graphviews-ui/src/components/graph/graph/Graph.tsx
+++ graphviews-ui/src/components/graph/graph/Graph.tsx
@@ -1,5 +1,8 @@
 import { forwardRef, useEffect, useState } from "react";
 import "./GraphStyle.css";
+import GraphApi from "../api/GraphApi";
+import { Input, Space } from 'antd';
+const { TextArea } = Input;
 
 // --- props:
 
@@ -22,26 +25,32 @@ export default forwardRef((props: GraphProps, ref) => {
   // --- load:
 
   useEffect(() => {
-    console.log(props.id);
-    const graph = {
-      id: props.id,
-      name: "name?",
-      source: "source?"
-    };
-    setGraph(graph);
+    getGraph(props.id);
   }, []);
 
   // --- graph:
 
   const [graph, setGraph] = useState<GraphData|null>(null);
 
+  const getGraph = (id: string) => {
+    GraphApi.getGraph(id, (graph: GraphData|null) => {
+      setGraph(graph || null);
+    });
+  };
+
   // --- ui:
 
   return (
     <div>
-      Graph: {props.id}
-      <div>{graph?.name}</div>
-      <div>{graph?.source}</div>
+      {graph && <>
+        <div>Name:</div>
+        <div className="graph_name">
+          <Input defaultValue={graph.name}/>
+        </div>
+        <div>Source:</div>
+        <div className="graph_source">
+          <TextArea autoSize defaultValue={graph.source}></TextArea></div>
+      </>}
     </div>
   );
 
