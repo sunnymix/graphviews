5e70464a72915b7eca817bf0d302f16c6a46562b
sunnymix <sunny@sunnymix.com>
2022-09-25 23:16:26 +0800

    显式命名Graph Controller依赖的服务

--- graphviews-api/src/main/java/com/sunnymix/graphviews/api/GraphController.java
+++ graphviews-api/src/main/java/com/sunnymix/graphviews/api/GraphController.java
@@ -1,7 +1,6 @@
 package com.sunnymix.graphviews.api;
 
 import com.sunnymix.graphviews.common.io.Out;
-import com.sunnymix.graphviews.dao.GraphDao;
 import com.sunnymix.graphviews.data.GraphUpdateForm;
 import com.sunnymix.graphviews.orm.jooq.tables.pojos.Graph;
 import com.sunnymix.graphviews.service.graph.*;
@@ -18,54 +17,54 @@ import java.util.Optional;
 public class GraphController {
 
     @Autowired
-    private GraphQueryService queryService;
+    private GraphQueryService graphQueryService;
 
     @Autowired
-    private GraphUpdateService updateService;
+    private GraphUpdateService graphUpdateService;
 
     @Autowired
-    private GraphCreateService createService;
+    private GraphCreateService graphCreateService;
 
     @Autowired
-    private GraphDeleteService deleteService;
+    private GraphDeleteService graphDeleteService;
 
     @Autowired
-    private GraphCopyService copyService;
+    private GraphCopyService graphCopyService;
 
     @GetMapping("/graph/query")
     public Out<List<Graph>> query(@RequestParam(name = "keyword", required = false) String keyword) {
-        List<Graph> graphs = queryService.query(keyword);
+        List<Graph> graphs = graphQueryService.query(keyword);
         return Out.ok(graphs);
     }
 
     @GetMapping("/graph/get/{id}")
     public Out<Graph> get(@PathVariable("id") String id) {
-        Optional<Graph> graph = queryService.get(id);
+        Optional<Graph> graph = graphQueryService.get(id);
         return Out.ok(graph);
     }
 
     @PostMapping("/graph/update/{id}")
     public Out<Void> update(@PathVariable("id") String id,
                             @RequestBody GraphUpdateForm form) {
-        Boolean updateSuccess = updateService.update(id, form);
+        Boolean updateSuccess = graphUpdateService.update(id, form);
         return Out.of(updateSuccess);
     }
 
     @PostMapping("/graph/create")
     public Out<String> create() {
-        String id = createService.create();
+        String id = graphCreateService.create();
         return Out.ok(id);
     }
 
     @PostMapping("/graph/delete/{id}")
     public Out<Void> delete(@PathVariable("id") String id) {
-        Boolean deleteSuccess = deleteService.delete(id);
+        Boolean deleteSuccess = graphDeleteService.delete(id);
         return Out.of(deleteSuccess);
     }
 
     @PostMapping("/graph/copy/{id}")
     public Out<String> copy(@PathVariable("id") String id) {
-        Optional<String> newId = copyService.copy(id);
+        Optional<String> newId = graphCopyService.copy(id);
         return newId.map(Out::ok).orElse(Out.error());
     }
 
